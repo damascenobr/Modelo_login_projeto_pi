1b72375369e978899227fa72f0d526b2aaeed16d
Jorge Damasceno <damascenotech87@gmail.com>
2025-05-04 20:56:55 -0300

    Corrigindo índice e commitando alterações

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('btnInteresse').addEventListener('click', function () {
+    alert('Seu interesse foi registrado! Entraremos em contato quando novas turmas forem abertas.');
+  });
+  
